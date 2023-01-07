edc6756df4b8aa593c3dd7ee9031793a7e8bfbdb
ayush7088 <ayushjoshi282@gmail.com>
2023-01-07 15:20:28 +0530

    updated-fun-translation-app

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# The-pig-latin-translator
- 
+# Pig-Latin Dicton Translator App
+
+## About Project:
+
+Do you want to transalte english to pig-latin. This simple application translates your english sentence to pig-latin.
+Enter your sentence in the textbox and with the click of the translate button, see the funny translation in the output box.
+
+## Tech Stack used:
+1. HTML 
+2. JavaScript
+3. CSS
+
+## GUI Sample.
+
+![image](https://user-images.githubusercontent.com/56500938/208805556-83f546fc-a0c1-4271-aeb7-f9b594d5900b.png)
+
+## LINK OF THE PROJECT
+You can also jump to the application from -->[link](https://pig-latin-app-ayush7088.netlify.app/).
---
+++ app.js
@@ -0,0 +1,34 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/pig-latin.json"
+function getTranslationURL(text)
+{
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error)
+{
+    console.log("Error occured", error);
+    alert("Something wrong with server, try again after sometime");
+}
+
+
+
+function clickHandler() {
+    // console.log("clicked");
+    // console.log("input", txtInput.value);
+    
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+            })
+        .catch(errorHandler)
+    
+};
+
+btnTranslate.addEventListener("click", clickHandler)
